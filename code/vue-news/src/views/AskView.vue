ec39395f832e291313aec57e397cda8e12a9384d
조준형 <cjh951114@naver.com>
2021-04-29 09:09:38 +0900

    05. API구현

--- code/vue-news/src/views/AskView.vue
+++ code/vue-news/src/views/AskView.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>ASK</div>
+    <div>
+        <div v-for="(item, index) in asks" :key="index">{{ item.title }}</div>
+    </div>
 </template>
 
 <script>
-export default {};
+import { fetchAskList } from '../api/index.js';
+export default {
+    data() {
+        return {
+            asks: [],
+        };
+    },
+    cerated() {
+        fetchAskList()
+            .then((response) => (this.ask = response.data))
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+};
 </script>
 
 <style></style>
--- code/vue-news/src/views/NewsView.vue
+++ code/vue-news/src/views/NewsView.vue
@@ -1,9 +1,40 @@
 <template>
-    <div>NEWS</div>
+    <div>
+        <div v-for="(user, index) in users" :key="index">{{ user.title }}</div>
+    </div>
 </template>
 
 <script>
-export default {};
+// import axios from 'axios';
+import { fetchNewsList } from '../api/index.js';
+
+export default {
+    data() {
+        return {
+            users: [],
+        };
+    },
+    created() {
+        // const url = 'https://api.hnpwa.com/v0/news/1.json';
+        // axios
+        //     .get(url)
+        //     .then((response) => {
+        //         console.log(response);
+        //         this.users = response.data;
+        //     })
+        //     .catch(() => {
+        //         console.log('error');
+        //     });
+        fetchNewsList()
+            .then((response) => {
+                console.log(response);
+                this.users = response.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+};
 </script>
 
 <style></style>
