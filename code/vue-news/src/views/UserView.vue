78e3fa6e02e978fec1b1e661ebd6a5506bdbdb08
조준형 <cjh951114@naver.com>
2021-05-02 18:13:04 +0900

    10. 리팩토링 2 사용자 프로필 컴포넌트 공통화
    10. 리팩토링 2 사용자 프로필 컴포넌트 공통화

--- code/vue-news/src/views/UserView.vue
+++ code/vue-news/src/views/UserView.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-        <p>name : {{ userInfo.id }}</p>
-        <p>karma : {{ userInfo.karma }}</p>
-        <p>created : {{ userInfo.created }}</p>
-    </div>
+    <user-profile :info="userInfo">
+        <div slot="username">{{ userInfo.id }}</div>
+        <span slot="time">{{ 'Joined ' + userInfo.created }}, </span>
+        <span slot="karma">{{ userInfo.karma }}</span>
+    </user-profile>
+    <!-- <user-profile></user-profile> -->
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
 export default {
+    components: {
+        UserProfile,
+    },
     computed: {
         userInfo() {
             return this.$store.state.user;
@@ -18,7 +23,6 @@ export default {
 
         const userName = this.$route.params.id;
 
-        // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
         this.$store.dispatch('FETCH_USER', userName);
     },
 };
